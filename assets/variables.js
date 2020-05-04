96b0ba82d02ebcc6e970aa44d2240213a13a20f1
nikiindianto <64794718+nikiindianto@users.noreply.github.com>
2020-05-05 00:07:18 +0700

    Update variables.js

--- assets/variables.js
+++ assets/variables.js
@@ -1,5 +1,9 @@
 export default {
+<<<<<<< HEAD
   nicknames: ["SALMA ALRILIA PUTRI"],
+=======
+  nicknames: "Salma Aprilia Putri", "Salma Baby Face", "Salma Cantik", "Salma Imut", "Salma Chan"],
+>>>>>>> e1d5ace381d5c76c490eb6425b6446a55391ae43
   greetings: {
     evening: "Good Evening",
     afternoon: "Good Afternoon",

ffc08645e304a776af8db2a1b4c5c3754b7ce225
nikiindianto <64794718+nikiindianto@users.noreply.github.com>
2020-05-05 01:28:51 +0700

    Update quotes.js

--- assets/quotes.js
+++ assets/quotes.js
@@ -1,10 +1,10 @@
 // prettier-ignore
 export default [
-    { author: "avrill.v", quotes: "Semangat belajarnya" },
-    { author: "antjedtta_", quotes: "Jangan putus asa" },
-    { author: "standinshd", quotes: "Ingat cita cita mu" },
-    { author: "standinshd", quotes: "Ingat harus banggain orang tua" },
-    { author: "standinshd", quotes: "Jangan Ngebucin wae :v" },
-    { author: "standinshd", quotes: "Nanti aja ngebucinnya" },
-    { author: "standinshd", quotes: "I LOVE YOU" },
+    { author: "standinshd", quotes: "Semangat Ujiannya" },
+    { author: "standinshd", quotes: "Semangat Ujiannya" },
+    { author: "standinshd", quotes: "Semangat Ujiannya" },
+    { author: "standinshd", quotes: "Semangat Ujiannya" },
+    { author: "standinshd", quotes: "Semangat Ujiannya" },
+    { author: "standinshd", quotes: "Semangat Ujiannya" },
+    { author: "standinshd", quotes: "Semangat Ujiannya" },
 ];

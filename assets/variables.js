069c7d59371569cd54f4dbce42e57e5a7540f1b2
nikiindianto <64794718+nikiindianto@users.noreply.github.com>
2020-05-05 08:25:24 +0700

    Update variables.js

--- assets/variables.js
+++ assets/variables.js
@@ -1,5 +1,5 @@
 export default {
-  nicknames: ["Habib Musthofa", "Habib X khadijah", "Habib X Ely", "Habib Bucin", "Habib lagi bucin"],
+  nicknames: ["Salma Aprilia Putri", "Salma chan", "Salma geulis", "Salma cantik", "Salma imut"],
   greetings: {
     evening: "Good Evening",
     afternoon: "Good Afternoon",

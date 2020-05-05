0c9d4cec6b148260d47d6b6156c235a5912ec2f5
nikiindianto <64794718+nikiindianto@users.noreply.github.com>
2020-05-05 12:15:33 +0700

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -38,7 +38,7 @@
         class="vue-typed"
         :showCursor="false"
         :loop="true"
-        :strings="[greetings() + ', ' + nicknames + '! ^5000', 'Hai gadis berkerudung merah' :v ^2500', 'Jangan Menyerah']"
+        :strings="[greetings() + ', ' + nicknames + '! ^5000', 'Hai gadis berkerudung merah:v ^2500', 'Jangan Menyerah']"
         :backDelay="500"
         :typeSpeed="100"
         :backSpeed="20"

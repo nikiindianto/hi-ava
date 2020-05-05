f186bd82bc7d75e00099edf7ced86e0be573d46d
nikiindianto <64794718+nikiindianto@users.noreply.github.com>
2020-05-05 07:48:18 +0700

    Update quotes.js

--- assets/quotes.js
+++ assets/quotes.js
@@ -1,10 +1,10 @@
 // prettier-ignore
 export default [
-    { author: "By. Niki Indianto", quotes: "Semangat Ujiannya" },
-    { author: "By. Niki Indianto", quotes: "Semangat Ujiannya" },
-    { author: "By. Niki Indianto", quotes: "Semangat Ujiannya" },
-    { author: "By. Niki Indianto", quotes: "Semangat Ujiannya" },
-    { author: "By. Niki Indianto", quotes: "Semangat Ujiannya" },
-    { author: "By. Niki Indianto", quotes: "Semangat Ujiannya" },
-    { author: "By. Niki Indianto", quotes: "Semangat Ujiannya" },
+    { author: "By. Niki Indianto", quotes: "Yang kuat Puasa nya" },
+    { author: "By. Niki Indianto", quotes: "Yang kuat Puasa nya' },
+    { author: "By. Niki Indianto", quotes: "Yang kuat Puasa nya" },
+    { author: "By. Niki Indianto", quotes: "Yang kuat Puasa nya" },
+    { author: "By. Niki Indianto", quotes: "Yang kuat Puasa nya" },
+    { author: "By. Niki Indianto", quotes: "Yang kuat Puasa nya" },
+    { author: "By. Niki Indianto", quotes: "Yang kuat Puasa nya" },
 ];
